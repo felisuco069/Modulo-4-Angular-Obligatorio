6ecc8fa40c9cdb5d7f34539e3f812d5e216b1dc7
felisuco069 <70918916+felisuco069@users.noreply.github.com>
2022-02-21 12:08:03 +0100

    intentando poner user y boton logout en cabecera

--- src/app/header-page/header-page.component.ts
+++ src/app/header-page/header-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-header-page',
   templateUrl: './header-page.component.html',
-  styleUrls: ['./header-page.component.scss']
+  styleUrls: ['./header-page.component.scss'],
 })
 export class HeaderPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public username = '';
+  public userLogued = false;
+  constructor(public service: AuthService) {
+    this.username = this.service.dataUser.username;
+    this.userLogued = this.service.userLogued;
   }
 
+  ngOnInit(): void {}
+  logout() {
+    this.service.logout();
+  }
 }
